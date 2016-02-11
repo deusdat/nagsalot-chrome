1aa2ce53a628a48a157e73abbd1e2e2d94ffe8cd
J Patrick Davenport <patrick.davenport@deusdatsolutions.com>
2016-02-10 23:07:20 -0500

    Adds in missing functions.

--- src/nagsalot/data.cljs
+++ src/nagsalot/data.cljs
@@ -1,6 +1,19 @@
 (ns nagsalot.data
   (:require [khroma.log :as console]))
 
+(def local js/chrome.storage.local)
+
+(defn load  [])
+
+(defn save 
+  ([config]
+    (save config 
+          (fn[] 
+            (console/log "completed save " (.-lastError js/chrome.runtime)))))
+  ([config on-complete]
+    (let [js-obj (clj->js config)]
+       (.set local js-obj on-complete))
+    (.get local nil (fn[t] (console/log "Loaded " t )))))
 
 (defn init[]
   (.addListener js/chrome.storage.onChange (fn [changes, area] (console/log "Something changed " changes))))
